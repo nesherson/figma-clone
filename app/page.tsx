085852f14480ecbb039cdf2b16c017b290617a8d
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-02 22:46:33 +0200

    Add navbar, add user message to live cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-"use client"
+"use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/navbar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+    </main>
   );
 }
