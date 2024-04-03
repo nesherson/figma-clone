bbdbaa5e44e5af1601bd1c96e9da4d4494f75a87
Nešad Omeragić <nesad.omeragich27@gmail.com>
2024-04-03 23:32:20 +0200

    Add toolbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import Live from "@/components/Live";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import LeftSidebar from "@/components/sidebar/LeftSidebar";
 import RightSidebar from "@/components/sidebar/RightSidebar";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -14,9 +15,20 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>("rectangle");
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: ""
+  });
+
+  const handleActiveElement = (element: ActiveElement) => {
+    setActiveElement(element);
+
+    selectedShapeRef.current = element?.value as string;
+  }
 
   useEffect(() => {
-    const canvas = initializeFabric({ canvasRef, fabricRef});
+    const canvas = initializeFabric({ canvasRef, fabricRef });
 
     canvas.on("mouse:down", (options) => {
       handleCanvasMouseDown({
@@ -34,7 +46,9 @@ export default function Page() {
   }, []);
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement} />
       <section className="flex h-full flex-row">
         <LeftSidebar />
         <Live canvasRef={canvasRef} />
